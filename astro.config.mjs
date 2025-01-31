106ebcec662d5bcacb2e7bd24598ff06cc1cec85
xabi1000 <babelwado@gmail.com>
2025-01-31 23:09:37 +0100

    Actualización de configuración y scripts de despliegue: se ajustan parámetros de prerender y se mejora la importación de datos en varios componentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import deno from '@deno/astro-adapter'
 export default defineConfig({
   output: 'server',
   adapter: deno(),
+  build: {
+    assets: 'assets',
+    inlineStylesheets: 'always',
+  },
   // Opcional: Prefijo para el idioma por defecto
   i18n: {
     defaultLocale: 'en',
@@ -14,6 +18,11 @@ export default defineConfig({
     typescript: {
       strict: false, // Reduce el chequeo de tipos
       check: false, // Desactiva la verificación de tipos en el build
+    },
+  },
+  vite: {
+    build: {
+      assetsInclude: ['**/*.json'], // <-- Aquí
     },
   },
 })
